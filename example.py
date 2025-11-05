ac28e16f1b944bd5b6764962964af1f5aa86cf21
wuxiaobin <1059605256@qq.com>
2025-11-05 18:34:31 +0800

    first init

--- example.py
+++ example.py
@@ -4,8 +4,11 @@ import torch
 from tqdm import tqdm
 import shutil
 
+dummy_input = torch.ones(1, 3, 416, 384).cuda()
 
-onnx_path = 'ckpt/resnet18.onnx'
+
+
+onnx_path = '/home/wuxiaobin/mycode/tmp/export/ckpt/resnet18.onnx'
 engine_path = onnx_path.replace('.onnx', '.engine')
 
 trt_cfg = dict(
@@ -21,11 +24,24 @@ print('no engine do onnx2trt done')
 print('move to fp16')
 shutil.move(engine_path, engine_path.replace('.engine', '_fp16.engine'))
 
-
-dummy_input = torch.ones(1, 3, 416, 384).cuda()
-_ = model(dummy_input)
-
+for _ in tqdm(range(10000), f"fp16"):
+    _ = model(dummy_input)
 
 
-for _ in tqdm(range(10000)):
+
+
+trt_cfg = dict(
+    max_batch_size=1,
+    fp16_mode=True,
+    fp8_mode=True,
+    int8_mode=False,
+)
+print('no engine do onnx2trt')
+model = TRTModel(onnx_path, **trt_cfg)
+print('no engine do onnx2trt done')
+
+print('move to fp8')
+shutil.move(engine_path, engine_path.replace('.engine', '_fp8.engine'))
+
+for _ in tqdm(range(10000), f"fp8"):
     _ = model(dummy_input)
